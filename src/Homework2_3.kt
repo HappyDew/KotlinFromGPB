1ab1af4740fe04dc2ca3ccae3d2013d06bfed896
HappyDew <mygpec@mail.ru>
2026-01-20 19:31:42 +0300

    Homework 20.01

---
+++ src/Homework2_3.kt
@@ -0,0 +1,60 @@
+import kotlin.time.Duration
+
+fun main() {
+    println("Задача 1")
+    //создали переменную serverStatus = String
+    var serverStatus = "processing"
+    //создали переменную countServerStatus равную нулю
+    var countServerStatus: Int = 0
+
+    //когда в цикле countServerStatus дойдет до 3 то остановится,
+    //а до этого печатает текст serverStatus
+    while (countServerStatus < 3) {
+        println("Статус: $serverStatus")
+        //инкремент чтобы цикл не стал вечным
+        countServerStatus++
+    }
+    //по завершению цикла переменная изменяется и печать последнего статуса
+    serverStatus = "success"
+    println("Статус: $serverStatus")
+
+    println("\nЗадача 2")
+    //цикл повторяется пять раз
+    for (testStart in 1..5) {
+        //создаем переменную i чтобы хранил в себе все значения
+        val i = when (testStart) {
+            1 -> "1"
+            2 -> "2"
+            3 -> "3"
+            4 -> "4"
+            else -> "5"
+        }
+        //пока цикл не дошел до 4 печатаем статичный текст три раза
+        if (i != "4" && i != "5") {
+            println("Тест $i: В процессе")
+            //как только дойдет до 4 печатает этот текст
+        } else if(i == "4") {
+            println("Тест 4: Провален")
+            //ну и в остальных случаях печатаем этот текст
+        } else {
+            println("Тест 5: Успех")
+        }
+    }
+
+    println("\nЗадача 3")
+    //функция которая определяет сложно теста на основе длительности и сложности
+    fun testBrains(duration: Int, complexity: Int){
+        when {
+            //когда тест до 10 минут то такой ответ
+            duration < 10 -> println("Быстрый тест")
+            //когда тест от 10 до 30 минут и сложность от 1 до 3 то такой ответ
+            duration in 10..30 && complexity in 1..3 -> println("Стандартный тест")
+            //когда тест от 10 до 30 минут и сложность от 4 до 5 то такой ответ
+            duration in 10..30 && complexity in 4..5 -> println("Сложный тест")
+            //когда тест более 30 минут такой ответ
+            duration > 30 -> println("Длительный тест")
+            //в дз не было про это, но добавил на всякий
+            else -> println("Тест провален")
+        }
+    }
+}
